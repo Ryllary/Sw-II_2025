0c9330fd48c2fad88fdec5d864dae0cf6edebae5
ryllary <victoriaryllary103@gmail.com>
2025-02-25 10:49:44 -0300

    save

---
+++ aula_03/exemplo5.php
@@ -0,0 +1,6 @@
+<?php
+
+    function msg($x){
+        $a = "Ryllary" .
+    }
+?>
---
+++ aula_03/exemplo6.php
@@ -0,0 +1,12 @@
+<?php
+
+    function teste($x,$y){
+        echo "Valor ' => $x <br>";
+        echo "Valor ' => $y <br>";
+    }
+    $a = 10;
+    $b = 20;
+
+    teste($a,$b);
+
+?>
